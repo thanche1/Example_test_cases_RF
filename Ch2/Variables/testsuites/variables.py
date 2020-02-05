2b57588fa41a109b5a4923c2f78b0c6100d85824
Athanasios Tselepidis <athanasios.tselepidis@siticom.de>
2020-02-05 15:54:04 +0100

    first commit

---
+++ Ch2/Variables/testsuites/variables.py
@@ -0,0 +1,6 @@
+# This file contains variable definitions
+Weather = [ "London", "Cloudy", 25]
+humidity = 75
+MARKS = 65.5
+DAYS = ["Monday", "Wednesday", "Friday", "Sunday"]
+details = {'genre':'open-source','type':'test automation', 'pub':2013}
